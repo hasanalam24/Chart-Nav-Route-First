76646e218a39e91f6fdb25a9d6cf868f2e7f470e
hasanalam24 <hasanalam365@gmail.com>
2024-03-21 00:23:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Rechart from './Components/Rechart.jsx/Rechart'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <Rechart></Rechart>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Rechart.jsx/Rechart.jsx
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const Rechart = () => {
+
+    const data = [
+        {
+            name: 'Page A',
+            marks: 70,
+        },
+        {
+            name: 'Page B',
+            marks: 90,
+        },
+        {
+            name: 'Page C',
+            marks: 56,
+        },
+        {
+            name: 'Page D',
+            marks: 37,
+        },
+        {
+            name: 'Page E',
+            marks: 66,
+        },
+        {
+            name: 'Page F',
+            marks: 88,
+        }
+    ]
+    return (
+        <div>
+            <BarChart width={500} height={300} data={data}
+
+            >
+
+                <CartesianGrid strokeDasharray="3 5" />
+                <XAxis dataKey="name" />
+
+                <YAxis />
+
+                <Tooltip></Tooltip>
+                <Bar dataKey="marks" barSize={30} fill="red" />
+
+            </BarChart>
+        </div>
+    );
+};
+
+export default Rechart;
